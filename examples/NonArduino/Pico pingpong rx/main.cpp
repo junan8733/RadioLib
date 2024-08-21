60a303a71799986193c0324300eea332cb2296d9
junan8733 <junan8733@gmail.com>
2024-08-21 20:36:44 +0900

    bidirectional_blink

--- examples/NonArduino/Pico pingpong rx/main.cpp
+++ examples/NonArduino/Pico pingpong rx/main.cpp
@@ -79,7 +79,55 @@ void setFlag(void) {
   operationDone = true;
 }
 
+// Pico W devices use a GPIO on the WIFI chip for the LED,
+// so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
+#ifdef CYW43_WL_GPIO_LED_PIN
+#include "pico/cyw43_arch.h"
+#endif
+
+#ifndef LED_DELAY_MS
+#define LED_DELAY_MS 250
+#endif
+
+// Perform initialisation
+int pico_led_init(void) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
+    // so we can use normal GPIO functionality to turn the led on and off
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    return PICO_OK;
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // For Pico W devices we need to initialise the driver etc
+    return cyw43_arch_init();
+#endif
+}
+
+// Turn the led on or off
+void pico_set_led(bool led_on) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    // Just set the GPIO on or off
+    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // Ask the wifi "driver" to set the GPIO on or off
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+#endif
+}
+
+void led_blink(int blink_count){
+  for (int i = 0; i < blink_count; i++){
+    pico_set_led(true);
+    sleep_ms(LED_DELAY_MS);
+    pico_set_led(false);
+    sleep_ms(LED_DELAY_MS);
+  }
+}
+
 int main() {
+  // initialize PICO LED
+  int rc = pico_led_init();
+  hard_assert(rc == PICO_OK);
+
   // initialize just like with Arduino
   printf("[LLCC68] Initializing ... ");
   int state = radio.begin();
@@ -153,7 +201,7 @@ int main() {
         transmitFlag = false;
 
         // 5초 딜레이.
-        hal->delay(5000);
+        //hal->delay(5000);
 
       } 
       else {
@@ -181,6 +229,8 @@ int main() {
           printf("%f", radio.getSNR());
           printf(" dB\n");
 
+          led_blink(5);
+
         }
 
         // wait a second before transmitting again 원래는 여기가 1초였다.
@@ -194,7 +244,10 @@ int main() {
         snprintf(str_to_transmit, sizeof(str_to_transmit), "Hello World! %d", count);
         transmissionState = radio.startTransmit(str_to_transmit);
         transmitFlag = true;
-      }      
+      }
+
+      // 10초 딜레이.
+      hal->delay(10000);
 
     }
 
--- examples/NonArduino/Pico pingpong tx/main.cpp
+++ examples/NonArduino/Pico pingpong tx/main.cpp
@@ -79,7 +79,55 @@ void setFlag(void) {
   operationDone = true;
 }
 
-int main() {
+// Pico W devices use a GPIO on the WIFI chip for the LED,
+// so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
+#ifdef CYW43_WL_GPIO_LED_PIN
+#include "pico/cyw43_arch.h"
+#endif
+
+#ifndef LED_DELAY_MS
+#define LED_DELAY_MS 250
+#endif
+
+// Perform initialisation
+int pico_led_init(void) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
+    // so we can use normal GPIO functionality to turn the led on and off
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    return PICO_OK;
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // For Pico W devices we need to initialise the driver etc
+    return cyw43_arch_init();
+#endif
+}
+
+// Turn the led on or off
+void pico_set_led(bool led_on) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    // Just set the GPIO on or off
+    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // Ask the wifi "driver" to set the GPIO on or off
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+#endif
+}
+
+void led_blink(int blink_count){
+  for (int i = 0; i < blink_count; i++){
+    pico_set_led(true);
+    sleep_ms(LED_DELAY_MS);
+    pico_set_led(false);
+    sleep_ms(LED_DELAY_MS);
+  }
+}
+
+int main() {  
+  // initialize PICO LED
+  int rc = pico_led_init();
+  hard_assert(rc == PICO_OK);
+
   // initialize just like with Arduino
   printf("[LLCC68] Initializing ... ");
   int state = radio.begin();
@@ -153,7 +201,7 @@ int main() {
         transmitFlag = false;
 
         // 5초 딜레이.
-        hal->delay(5000);
+        //hal->delay(5000);
 
       } 
       else {
@@ -180,6 +228,8 @@ int main() {
           printf("[LLCC68] SNR:\t\t");
           printf("%f", radio.getSNR());
           printf(" dB\n");
+          
+          led_blink(5);
 
         }
 
@@ -195,6 +245,9 @@ int main() {
         transmissionState = radio.startTransmit(str_to_transmit);
         transmitFlag = true;
       }
+
+      // 10초 딜레이.
+      hal->delay(10000);
 
     }
 
